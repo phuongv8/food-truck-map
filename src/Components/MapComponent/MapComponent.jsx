9ab65d970f6dabdabe036a5ba76f097a1d065d4d
Phuong Vu <100975883+phuongv8@users.noreply.github.com>
2023-05-19 11:04:50 -0700

    Change marker default icon, display markers on map

--- src/Components/MapComponent/MapComponent.jsx
+++ src/Components/MapComponent/MapComponent.jsx
@@ -1,6 +1,5 @@
-import L from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { icon } from 'leaflet';
+import L from 'leaflet';
 import './MapComponent.css';
 
 const MapComponent = ({ foodTrucks }) => {
@@ -8,13 +7,12 @@ const MapComponent = ({ foodTrucks }) => {
   const zoom = 13;
 
   const defaultIcon = L.icon({
-    iconUrl: 'https://leafletjs.com/examples/custom-icons/leaf-green.png',
-    shadowUrl: 'https://leafletjs.com/examples/custom-icons/leaf-shadow.png',
-    iconSize: [38, 95], // size of the icon
-    shadowSize: [50, 64], // size of the shadow
-    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62], // the same for the shadow
-    popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    iconSize: [24, 40],
+    shadowSize: [41, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
   });
 
   return (
@@ -27,21 +25,20 @@ const MapComponent = ({ foodTrucks }) => {
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        >
-          {foodTrucks.map((truck, index) => (
-            <Marker
-              key={index}
-              position={[truck.Latitude, truck.Longitude]}
-              icon={defaultIcon}
-            >
-              <Popup>
-                <strong>{truck.Applicant}</strong>
-              </Popup>
+        />
+        {foodTrucks.map((truck, index) => (
+          <Marker
+            key={index}
+            position={[truck.Latitude, truck.Longitude]}
+            icon={defaultIcon}
+          >
+            <Popup>
+              <strong>{truck.Applicant}</strong>
               <br />
               {truck.FoodItems}
-            </Marker>
-          ))}
-        </TileLayer>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
